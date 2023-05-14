66b2adde365d31ef7f6b93c6b8e747b4bf140342
saagarbhujel <saagarbhujel22@gmail.com>
2023-05-14 18:25:53 +0545

    navbar style done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
 
 import './App.css'
+import NavBar from './Components/navbar/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <NavBar/>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/Components/navbar/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const NavBar = () => {
+  return (
+    <>
+      <Navbar bg="dark" expand="lg" className="navbar navbar-dark">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#business">Business</Nav.Link>
+              <Nav.Link href="#entertainment">Entertainment</Nav.Link>
+              <Nav.Link href="#general">General</Nav.Link>
+              <Nav.Link href="#health">Health</Nav.Link>
+              <Nav.Link href="#science">Science</Nav.Link>
+              <Nav.Link href="#sports">Sports</Nav.Link>
+              <Nav.Link href="#technology">Technology</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
