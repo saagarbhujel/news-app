c9a6760fbeeed4f6b49c6f02bee19a97e9d3e16d
saagarbhujel <saagarbhujel22@gmail.com>
2023-05-14 18:27:20 +0545

    news comp

---
+++ src/Components/newsbody/NewsComp.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import NewsItems from "./NewsItems";
+import axios from "axios";
+// import PropTypes from "prop-types";
+
+const NewsComp = ({ pageSize }) => {
+  const [articles, setArticle] = useState([]);
+
+  const fetchArticle = async () => {
+    const postData = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=2e3ca7aaebd54af29bff4e9588109119&page=1&pageSize=${pageSize}`
+      //
+      //    {
+      //   headers: {
+      //       apiKey: "2e3ca7aaebd54af29bff4e9588109119",
+      //     },
+      //   }
+    );
+    setArticle(postData.data.articles);
+    console.log(postData.data);
+  };
+
+  useEffect(() => {
+    fetchArticle();
+  }, []);
+  return (
+    <>
+      <div className="container ">
+        <div className="row">
+          {articles.map((e, index) => {
+            return (
+              <div className="col-md-3 my-4" key={index}>
+                <NewsItems
+                  title={e.title ? e.title.slice(0, 40) : ""}
+                  description={e.description ? e.description.slice(0, 75) : ""}
+                  newsUrl={e.url}
+                  imgUrl={
+                    e.urlToImage
+                      ? e.urlToImage
+                      : "https://techcrunch.com/wp-content/uploads/2023/04/GettyImages-1367534680.jpg?resize=1200,765"
+                  }
+                  author={e.author}
+                  date={e.publishedAt}
+                  source={e.source.name}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+// NewsComp.defaultProps = {
+//     country: "in",
+//     pageSize: "8",
+//     category: "general",
+//   };
+
+//  NewsComp.propTypes = {
+//     country: PropTypes.string,
+//     pageSize: PropTypes.number,
+//     category: PropTypes.string,
+//   };
+
+export default NewsComp;
---
+++ src/Components/newsbody/NewsItems.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./newsitems.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+const NewsItems = ({
+  title,
+  description,
+  imgUrl,
+  newsUrl,
+  author,
+  date,
+  source,
+}) => {
+  return (
+    <>
+      <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={imgUrl} style={{ height: 200 }} />
+          <Card.Body>
+            <Card.Title>{title}... </Card.Title>
+            <Card.Text>{description}...</Card.Text>
+            <p>
+              By: {author} on {new Date(date).toGMTString()}{" "}
+            </p>
+            <a href={newsUrl}>
+              <Button variant="primary">Read More</Button>
+            </a>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default NewsItems;
